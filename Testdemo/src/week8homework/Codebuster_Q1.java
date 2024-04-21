b2777fdd943b31f41bbeddf5318ced7c536fb2a6
Joshkriti <Komalkanji13@gmail.com>
2024-04-21 22:37:50 +0100

    First push

---
+++ Testdemo/src/week8homework/Codebuster_Q1.java
@@ -0,0 +1,20 @@
+package week8homework;
+
+public class Codebuster_Q1 {
+    /*
+    1. Write a Java program to create a class called codebuster with a method called
+groupName(). Create a subclass called codebusterGroups() that overrides the
+groupName() method.
+     */
+
+    public static void main(String[] args) {
+        Codebuster_Q1 C = new Codebuster_Q1();
+        C.groupName();
+
+
+    }
+
+    public void groupName(){
+        System.out.println("Code1");
+    }
+}
---
+++ Testdemo/src/week8homework/Sub_class_Q1.java
@@ -0,0 +1,21 @@
+package week8homework;
+
+public class Sub_class_Q1 extends Codebuster_Q1{
+
+    public static void main(String[] args) {
+        Sub_class_Q1 S = new Sub_class_Q1();
+        S.codebustorGroups();
+        S.groupName();
+
+    }
+
+    @Override
+    public void groupName() {
+        System.out.println("Code2");
+    }
+
+    public void codebustorGroups(){
+        System.out.println("Code1, Code2, Code3");
+    }
+
+}
