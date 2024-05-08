5c73ff357f1a25be8fb03e2929b1344b6384bb47
Joshkriti <Komalkanji13@gmail.com>
2024-05-08 22:13:09 +0100

    First push

---
+++ Testdemo/src/week8homework2/Q18_SimpleCalculator.java
@@ -0,0 +1,63 @@
+package week8homework2;
+
+public class Q18_SimpleCalculator {
+
+    double firstNumber;
+    double secondNumber;
+
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0;
+        } else {
+            return firstNumber / secondNumber;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Q18_SimpleCalculator calculator = new Q18_SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+
+    }
+
+
+
+
+
+}
+
